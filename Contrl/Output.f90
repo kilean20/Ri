0f54ad018d9630d736748ccc409607a465a83919
Kilean20 <kilean20@gmail.com>
2019-05-07 20:44:58 -0700

    f to f90

--- Contrl/Output.f90
+++ Contrl/Output.f90
@@ -2654,31 +2654,31 @@
          endif       
         
         !<<<<<<<<<<<<<<<<<<<<<<<< openPMD <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-!        if(formatID==4) then
-!          if(abs(this%mass/proton_mass-1d0) < 0.001 .and. this%charge==1d0) then
-!            pName = 'proton' 
-!          elseif(abs(this%mass/electron_mass-1d0)<0.001) then
-!            if(this%charge==-1d0) then
-!              pName = 'electron' 
-!            elseif(this%charge==1d0) then
-!              pName = 'positron'
-!            else
-!              pName = 'unknown'
-!            endif
-!          else
-!            pName = 'unknown'
-!          endif
-!          ibetgam = 1d0/sqrt(this%refptcl(6)**2-1d0)
-!          betC = sqrt(1d0-(1d0/this%refptcl(6))**2)*cLight
-!          call openPMD_particle_output(this%Pts1,this%Nptlocal,nfile,iter,&
-!                                      &this%mass*eV_2_kg,pName,samplePeriod,&
-!                                      &[Scxl,ibetgam,Scxl,ibetgam,&
-!                                      & betC/(2d0*Pi*Scfreq),-this%mass ])
-!          return
-!        elseif(formatID==3) then
-!          call hdf5_particle_output(nfile,this%Pts1(1:9,1:this%Nptlocal),&
-!                                   &this%Nptlocal)
-!        endif
+        ! if(formatID==4) then
+          ! if(abs(this%mass/proton_mass-1d0) < 0.001 .and. this%charge==1d0) then
+            ! pName = 'proton' 
+          ! elseif(abs(this%mass/electron_mass-1d0)<0.001) then
+            ! if(this%charge==-1d0) then
+              ! pName = 'electron' 
+            ! elseif(this%charge==1d0) then
+              ! pName = 'positron'
+            ! else
+              ! pName = 'unknown'
+            ! endif
+          ! else
+            ! pName = 'unknown'
+          ! endif
+          ! ibetgam = 1d0/sqrt(this%refptcl(6)**2-1d0)
+          ! betC = sqrt(1d0-(1d0/this%refptcl(6))**2)*cLight
+          ! call openPMD_particle_output(this%Pts1,this%Nptlocal,nfile,iter,&
+                                      ! &this%mass*eV_2_kg,pName,samplePeriod,&
+                                      ! &[Scxl,ibetgam,Scxl,ibetgam,&
+                                      ! & betC/(2d0*Pi*Scfreq),-this%mass ])
+          ! return
+        ! elseif(formatID==3) then
+          ! call hdf5_particle_output(nfile,this%Pts1(1:9,1:this%Nptlocal),&
+                                   ! &this%Nptlocal)
+        ! endif
         !>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
@@ -4542,8 +4542,11 @@
         double precision :: xn,pxn,yn,pyn,Hinv,Iinv,gambet0,p_p0
         double precision, allocatable,dimension(:,:) :: recvbuf, sendbuf
         
+        
         call MPI_COMM_RANK(MPI_COMM_WORLD,my_rank,ierr)
         call MPI_COMM_SIZE(MPI_COMM_WORLD,np,ierr)
+        
+        if(my_rank==0) print*, 'fileID,beta,alfa,tn,cn,pIDbegin,pIDend=',fileID,beta,alfa,tn,cn,pIDbegin,pIDend
         gambet0 = sqrt(BB%refptcl(6)**2-1.0d0) 
         isTest = pIDbegin <= BB%Pts1(9,1:BB%Nptlocal) .and. &  ! inteded type cast. ignore compiler warining.
                  BB%Pts1(9,1:BB%Nptlocal) <= pIDend  
